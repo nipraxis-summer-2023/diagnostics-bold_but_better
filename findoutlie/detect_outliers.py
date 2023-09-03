bcf86bdae304a3f0a59b2127e5076c900ad59602
David Gudjonsson <david.gudjonsson@gmail.com>
2023-09-03 14:29:29 +0000

    added docstrings to detect_outliers.py functions

--- findoutlie/detect_outliers.py
+++ findoutlie/detect_outliers.py
@@ -7,13 +7,14 @@ import nibabel as nib
 
 
 def vol_mean(data):
-    """     Calculates the mean of the volume of the data
+    """
+    Calculates the mean of each volume in the 4D data
     
-    Parameters: a nibabel object
-    data: a nibable image 4D fMRI data with many volumes
+    Parameters: a nibabel image
+    data: a nibable image (nibabel.nifti1.Nifti1Image), a 4D fMRI data with volumes over time
     --------
     Returns: list
-    a list of means per volume in the data
+    a list of means, each item being mean per volume across time
     """
     return [
         np.mean(data[..., vol])
@@ -22,7 +23,13 @@ def vol_mean(data):
 
 def vol_std(data):
     """
-    Calculates the standard deviation of the volume of the data
+    Calculates the standard deviation of each volume in the 4D data
+
+    Parameters: nibable image
+    data: a nibable image (nibabel.nifti1.Nifti1Image), a 4D fMRI data with volumes over time
+    -------
+    Returns: list
+    A list of standard deviations, each itema being std per volume across time
     """
     return [
         np.std(data[..., vol])
@@ -31,7 +38,14 @@ def vol_std(data):
 
 def detect_outliers(data, n_std=2):
     """
-    Detects outliers in the data and returns a list of indices of the outliers
+    Detects outliers in the 4D data and returns a list of indices of outlier volumes
+
+    Parameters: nibable image, int
+    data: a nibable image (nibabel.nifti1.Nifti1Image), a 4D fMRI data with volumes over time
+    n_std: number of standard deviations away from mean for a volue to be classified as an outlier; default 2
+    -------
+    Returns: numpy array
+    A list of indices of outlier volumes in the data (classifed as > n_std from the mean)
     """
     means = vol_mean(data)
     mean_means = np.mean(means)
@@ -48,7 +62,15 @@ def detect_outliers(data, n_std=2):
 
 def remove_outliers(data, outliers):
     """
-    Removes the outliers from the data and returns the new data
+    Removes the outlier volumes in the 4D data and returns data without outliers
+    
+    Parameters: nibabel image, numpy array
+    data: a nibable image (nibabel.nifti1.Nifti1Image), a 4D 
+    outliers: A list of indices of outlier volumes in the data
+    ------
+    Returns: nibabel image data (numpy.memmap, memory map)
+    A 4D fMRI data volumes over time, without outlier volumes
+
     """
     # data is a 4D list, with [x, y, z, volume]
     return [
@@ -65,14 +87,20 @@ def remove_outliers(data, outliers):
 
 def main(file_path):
     """
-    Main function
+    This function (main) is called when this file run as a script.
+
+    Parameters: str
+    A path to nibable image file
+    Returns: numpy array
+    A list of indices of outlier volumes in the data   
     """
     data_fname = npx.fetch_file(file_path) # Fetches the file
     img = nib.load(data_fname) # Loads the file
-    data = img.get_fdata() # Gets the data from the file
+    data = img.get_fdata() # Gets the data from the image file
 
     outliers = detect_outliers(data)
-    return remove_outliers(data, outliers)
+    return outliers
+    # return remove_outliers(data, outliers)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
