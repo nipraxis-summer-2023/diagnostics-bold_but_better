7a086175c66d20e91706e78b5b14e4f492b75b91
David Gudjonsson <david.gudjonsson@gmail.com>
2023-09-28 15:08:24 +0200

    added dvars to __init__.py

--- findoutlie/__init__.py
+++ findoutlie/__init__.py
@@ -3,4 +3,4 @@
 __version__ = '0.1a0'
 
 from .outfind import find_outliers
-from .detectors import iqr_detector, z_score_detector, dvars_detector
+from .detectors import iqr_detector, z_score_detector, dvars, dvars_detector
